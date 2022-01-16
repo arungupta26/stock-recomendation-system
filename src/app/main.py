37c5a3edd63bd024ac53529a9af2f97aba6216ab
arungupta <arungupta26@yahoo.com>
2022-01-16 18:26:47 +0530

    added new page

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,5 @@
+# pip install streamlit fbprophet yfinance plotly
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,15 +8,35 @@ import yfinance as yf
 from tensorflow.keras.models import load_model
 import streamlit as st
 
-from datetime import date
+from datetime import 
+
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
 
 
 ### all functions
-
+@st.cache
 def load_data(ticker):
 	data = yf.download(ticker , START , TODAY)
 	data.reset_index(inplace=True)
 	return load_data
+
+
+
+# Plot raw data
+def plot_raw_data():
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+
+
+
+
+### all functions end
+
 
 
 
@@ -50,13 +72,32 @@ data= load_data(selected_stock)
 data_load_state.text("Loading data... Done.")
 
 
+st.subheader("Raw data")
+st.write(data.tail())
 
 
+plot_raw_data()
 
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
 
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+    
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
 
-
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
 
 
 
